8e8631f912f2afd1696e410f1e82c1ef182069ff
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-02-28 12:28:25 -0600

    ADD: new cambio

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 Server.get("/", (request, response) => {
     response.status(200);
-    response.send("<h1>Hola Server ⚙️ 2</h1>");
+    response.send("<h1>Hola Server ⚙️ 2 Glitch</h1>");
 });
 
 Server.listen(PORT, () => {
