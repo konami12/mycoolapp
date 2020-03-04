b513816d2e2a316c5b5a2da478a008ee0d5b4b42
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-03-04 02:20:45 -0600

    ADD: tema

---
+++ src/app/app.js
@@ -0,0 +1,17 @@
+import Express, { static as Statics } from "express";
+import Path from "path";
+
+const Server = Express();
+const PORT = process.env.PORT || 3000;
+const ROOT_PATH = Path.join(__dirname, "../");
+
+Server.use("/", Statics(`${ROOT_PATH}/public/`));
+
+Server.get("/", (request, response) => {
+    response.status(200);
+    response.send("<h1>Hola Server ⚙️ 2 Glitchs usando build 20</h1>");
+});
+
+Server.listen(PORT, () => {
+    console.log("Corriendo => ", PORT);
+});
